1efc6e75501820b64b8c3b75a645df7e0f11e578
Lt-grint <1598552894@qq.com>
2017-04-22 15:02:48 +0800

    admin模块添加AdminServlet
    admin模块添加AdminServlet

---
+++ ishop-admin/src/main/java/com/ishop/servlet/AdminServlet.java
@@ -0,0 +1,50 @@
+package com.ishop.servlet;
+
+import com.ishop.pojo.User;
+import com.ishop.util.Page;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by tao on 2017/4/22 0022.
+ */
+public class AdminServlet extends BaseBackServlet {
+
+    public String add(HttpServletRequest request, HttpServletResponse response, Page page) {
+
+        return null;
+    }
+
+
+    public String delete(HttpServletRequest request, HttpServletResponse response, Page page) {
+        return null;
+    }
+
+
+    public String edit(HttpServletRequest request, HttpServletResponse response, Page page) {
+        return null;
+    }
+
+
+    public String update(HttpServletRequest request, HttpServletResponse response, Page page) {
+        return null;
+    }
+
+
+    public String list(HttpServletRequest request, HttpServletResponse response, Page page) {
+        List<User> us = userDao.list(page.getStart(),page.getCount());
+        int total = userDao.getTotal();
+        page.setTotal(total);
+
+        request.setAttribute("us", us);
+        request.setAttribute("page", page);
+
+        return "admin/listUser.jsp";
+    }
+}
