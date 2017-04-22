494e2a1f611aabf4156c5f9c8a6b3803233aee08
Lt-grint <1598552894@qq.com>
2017-04-22 15:02:23 +0800

    admin模块添加BaseBaceServlet和BaseForeServlet
    admin模块添加BaseBaceServlet和BaseForeServlet

---
+++ ishop-admin/src/main/java/com/ishop/servlet/BaseForeServlet.java
@@ -0,0 +1,69 @@
+
+package com.ishop.servlet;
+
+
+import com.ishop.dao.*;
+import com.ishop.dao.impl.*;
+import com.ishop.util.Page;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
+
+public class BaseForeServlet extends HttpServlet{
+    protected TypeDao typeDao = new TypeDaoImpl();
+    protected AdminDao adminDao = new AdminDaoImpl();
+    protected UserDao userDao = new UserDaoImpl();
+    protected ProductDao productDao = new ProductDaoImpl();
+//	protected CategoryDAO categoryDAO = new CategoryDAO();
+//	protected OrderDAO orderDAO = new OrderDAO();
+//	protected OrderItemDAO orderItemDAO = new OrderItemDAO();
+//	protected ProductDAO productDAO = new ProductDAO();
+//	protected ProductImageDAO productImageDAO = new ProductImageDAO();
+//	protected PropertyDAO propertyDAO = new PropertyDAO();
+//	protected PropertyValueDAO propertyValueDAO = new PropertyValueDAO();
+//	protected ReviewDAO reviewDAO = new ReviewDAO();
+//	protected UserDAO userDAO = new UserDAO();
+	
+	public void service(HttpServletRequest request, HttpServletResponse response) {
+		try {
+			
+			int start= 0;
+			int count = 10;
+			try {
+				start = Integer.parseInt(request.getParameter("page.start"));
+			} catch (Exception e) {
+				
+			}
+			
+			try {
+				count = Integer.parseInt(request.getParameter("page.count"));
+			} catch (Exception e) {
+			}
+			
+			Page page = new Page(start,count);
+			
+			String method = (String) request.getAttribute("method");
+
+			Method m = this.getClass().getMethod(method, HttpServletRequest.class,
+					HttpServletResponse.class,Page.class);
+			
+			String redirect = m.invoke(this,request, response,page).toString();
+			
+			if(redirect.startsWith("@"))
+				response.sendRedirect(redirect.substring(1));
+			else if(redirect.startsWith("%"))
+				response.getWriter().print(redirect.substring(1));
+			else
+				request.getRequestDispatcher(redirect).forward(request, response);
+			
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+}
+
