8223e65a4355c194611ff03be0de389776b475c6
Lt-grint <1598552894@qq.com>
2017-04-23 19:46:22 +0800

    portal 添加中文乱码过滤器
    portal 添加中文乱码过滤器

---
+++ ishop-portal/src/main/java/com/ishop/portal/filter/CharSetFilter.java
@@ -0,0 +1,29 @@
+package com.ishop.portal.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by tao on 2017/4/3 0003.
+ */
+public class CharSetFilter implements Filter {
+    private String myEncoding;
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) resp;
+        request.setCharacterEncoding(myEncoding);
+        response.setCharacterEncoding(myEncoding);
+        chain.doFilter(req, resp);
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+        //从web.xml文件读取默认编码
+        this.myEncoding = config.getInitParameter("initEncoding");
+    }
+
+}
