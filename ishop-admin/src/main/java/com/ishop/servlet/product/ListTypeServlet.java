a6492ffef855c807ba49723e41198a7237c5dc1b
myq <84210278@qq.com>
2017-04-23 18:20:28 +0800

    add ListTypeServlet.java

---
+++ ishop-admin/src/main/java/com/ishop/servlet/product/ListTypeServlet.java
@@ -0,0 +1,44 @@
+package com.ishop.servlet.product;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.ishop.dao.TypeDao;
+import com.ishop.dao.impl.TypeDaoImpl;
+import com.ishop.pojo.Type;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+/**
+ * Created by myq on 2017/4/23.
+ */
+@WebServlet(name="ListTypeServlet",urlPatterns = "/listType")
+public class ListTypeServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try{
+            TypeDao pd = new TypeDaoImpl();
+            List<Type> list = pd.list();
+            PrintWriter out = response.getWriter();
+            String json = JSON.toJSONString(list, SerializerFeature.WriteDateUseDateFormat,SerializerFeature.PrettyFormat, SerializerFeature.DisableCircularReferenceDetect);
+            if(list != null) {
+                out.println(json);
+                out.close();
+            }else{
+                out.println("获取失败!");
+                out.close();
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request,response);
+    }
+}
