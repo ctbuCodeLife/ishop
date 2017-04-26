0f3b965468e5fd76c0d10256dcb6ddd105c7c897
Lt-grint <1598552894@qq.com>
2017-04-26 16:04:57 +0800

    admin 模块管理员管理优化提示
    完成删除管理员,添加管理员提示

--- ishop-admin/src/main/webapp/admin/addAdmin.jsp
+++ ishop-admin/src/main/webapp/admin/addAdmin.jsp
@@ -10,6 +10,8 @@
 <head>
     <title>添加管理员</title>
     <link rel="stylesheet" href="<%=request.getContextPath()%>/css/bootstrap.min.css">
+    <!--提示框插件样式-->
+    <link rel="stylesheet" href="<%=request.getContextPath()%>/css/sweetalert2.min.css">
     <style type="text/css">
         .container{
             width: 500px;
@@ -71,6 +73,11 @@
 </div>
 <script src="<%=request.getContextPath()%>/js/jquery.min.js"></script>
 <script src="<%=request.getContextPath()%>/js/vue.js"></script>
+<!--引入插件的js-->
+<script src="<%=request.getContextPath()%>/js/sweetalert2.min.js"></script>
+
+<!--自定义的管理admin的js-->
 <script src="<%=request.getContextPath()%>/js/admin.js"></script>
+
 </body>
 </html>
--- ishop-admin/src/main/webapp/js/admin.js
+++ ishop-admin/src/main/webapp/js/admin.js
@@ -4,42 +4,84 @@ function addAdmin() {
     var name = $("#name").val();
     var password = $("#password").val();
     var realName = $("#realName").val();
-    var email =  $("#email").val();
+    var email = $("#email").val();
     var phone = $("#phone").val();
+    var adminEle = ["#roleId", "#name", "#password", "#realName", "#email", "#phone"];
+    for (var i = 0; i < adminEle.length; i++) {
+        if ($(adminEle[i]).val() == "") {
+            $(adminEle[i]).focus();
+            return;
+        }
+    }
     $.ajax({
-        type:"GET",
-        url:"/ishop-admin/addAdmin",
-        data:{
-            roleId:roleId,
-            name:name,
-            password:password,
-            realName:realName,
-            email:email,
-            phone:phone
+        type: "POST",
+        url: "/ishop-admin/addAdmin",
+        data: {
+            roleId: roleId,
+            name: name,
+            password: password,
+            realName: realName,
+            email: email,
+            phone: phone
         },
-        dataType:"json",
-        success:function (data) {
+        dataType: "json",
+        success: function (data) {
             //这里获取到数据展示到前台
-            alert(data);
+
         }
     });
-    location.href="listAdmin.jsp";
+    swal(
+        '添加成功!',
+        '成功添加了一条管理员信息!',
+        'success'
+    )
+//2秒后自动跳转
+    function autoReturn() {
+        location = "listAdmin.jsp";
+    }
+
+    setTimeout(autoReturn, 2000);
 }
 //删除管理员
 function delAdmin(that) {
-    var p = that.parentNode.firstChild;
-    var id = p.innerHTML;
-    $.ajax({
-        type:"GET",
-        url:"/ishop-admin/delAdmin",
-        data:{id:id},
-        dataType:"json",
-        success:function (data) {
-            //这里获取到数据展示到前台
-            alert(data);
+    swal({
+        title: '删除管理员?',
+        text: "删除后就不可恢复!",
+        type: 'error',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: '确认删除',
+        cancelButtonText: "取消",
+    }).then(function (isConfirm) {
+        if (isConfirm) {
+            //执行删除操作
+            var p = that.parentNode.firstChild;
+            var id = p.innerHTML;
+            $.ajax({
+                type: "GET",
+                url: "/ishop-admin/delAdmin",
+                data: {id: id},
+                dataType: "json",
+                success: function (data) {
+                    //这里获取到数据展示到前台
+                    // alert(data);
+                }
+            });
+            swal(
+                '删除成功!',
+                '您已经成功删除管理员',
+                'success'
+            ).then(function (isConfirm) {
+                location.reload();
+            });
         }
     });
-    location.reload();
+    function autoReturn() {
+        location.reload();
+    }
+
+    //setTimeout(autoReturn,2000);
 }
 //修改管理员
 function updateAdmin() {
@@ -48,43 +90,44 @@ function updateAdmin() {
     var name = $("#name").val();
     var password = $("#password").val();
     var realName = $("#realName").val();
-    var email =  $("#email").val();
+    var email = $("#email").val();
     var phone = $("#phone").val();
     $.ajax({
-        type:"GET",
-        url:"/ishop-admin/updateAdmin",
-        data:{
-            id:id,
-            roleId:roleId,
-            name:name,
-            password:password,
-            realName:realName,
-            email:email,
-            phone:phone
+        type: "GET",
+        url: "/ishop-admin/updateAdmin",
+        data: {
+            id: id,
+            roleId: roleId,
+            name: name,
+            password: password,
+            realName: realName,
+            email: email,
+            phone: phone
         },
-        dataType:"json",
-        success:function (data) {
+        dataType: "json",
+        success: function (data) {
             //这里获取到数据展示到前台
             alert(data);
         }
     });
-    location.href="listAdmin.jsp";
+    location.href = "listAdmin.jsp";
 }
 //查看所有管理员
 function listAdmin() {
-    mydata=[];
+    mydata = [];
     $.ajax({
-        type:"GET",
-        url:"/ishop-admin/listAdmin",
-        dataType:"json",
-        success:function (data) {
+        type: "GET",
+        url: "/ishop-admin/listAdmin",
+        dataType: "json",
+        success: function (data) {
             //这里获取到数据展示到前台
             var vm = new Vue({
-                el:'#adminTable',
-                data:{
-                    mydata:data
+                el: '#adminTable',
+                data: {
+                    mydata: data
                 }
             });
+            $("#queryTable").addClass("show");
         }
     })
 }
@@ -93,16 +136,16 @@ function getAdmin() {
     var name = $("#name").val();
     $("#queryAdminTable").show();
     $.ajax({
-        type:"GET",
-        url:"/ishop-admin/getAdminByName",
-        data:{name:name},
-        dataType:"json",
-        success:function (data) {
+        type: "GET",
+        url: "/ishop-admin/getAdminByName",
+        data: {name: name},
+        dataType: "json",
+        success: function (data) {
             //这里获取到数据展示到前台
             var vm = new Vue({
-                el:'#queryAdminTable',
-                data:{
-                    admin:data
+                el: '#queryAdminTable',
+                data: {
+                    admin: data
                 }
             });
         }
