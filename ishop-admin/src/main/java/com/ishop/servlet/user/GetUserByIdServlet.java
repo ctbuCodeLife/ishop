f2a6e23f85c8c9515bc329cbe57ed8fc17365d69
myq <84210278@qq.com>
2017-04-28 19:33:09 +0800

    admin GetUserByIdServlet.java

---
+++ ishop-admin/src/main/java/com/ishop/servlet/user/GetUserByIdServlet.java
@@ -0,0 +1,46 @@
+package com.ishop.servlet.user;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.ishop.dao.UserDao;
+import com.ishop.dao.impl.UserDaoImpl;
+import com.ishop.pojo.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by tao on 2017/4/22 0022.
+ */
+@WebServlet(name = "GetUserByIdServlet")
+public class GetUserByIdServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            UserDao ud = new UserDaoImpl();
+            String paramId = request.getParameter("id");
+            Integer id = Integer.parseInt(paramId);
+            User user = ud.get(id);
+            PrintWriter out = response.getWriter();
+            //将list的数据转换成JSON返回给前台
+            //JSON.DEFFAULT_DATE_FORMAT = "yyyy-MM-dd";
+            //SerializerFeature.WriteDateUseDateFormat用来将日期格式化成yyyy-MM-dd的形式
+            String json = JSON.toJSONString(user, SerializerFeature.WriteDateUseDateFormat, SerializerFeature.PrettyFormat, SerializerFeature.DisableCircularReferenceDetect);
+            if (user != null) {
+                out.println(json);
+                out.close();
+            } else {
+                out.println("获取失败!");
+                out.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            doPost(request,response);
+    }
+}
