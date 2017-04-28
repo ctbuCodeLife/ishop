d902250d116e8e03298c61a5cb831bbd0670d354
Lt-grint <1598552894@qq.com>
2017-04-28 20:54:57 +0800

    admin 更新type相关

---
+++ ishop-admin/src/main/java/com/ishop/servlet/product/GetTypeByIdServlet.java
@@ -0,0 +1,48 @@
+package com.ishop.servlet.product;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.ishop.dao.TypeDao;
+import com.ishop.dao.impl.TypeDaoImpl;
+import com.ishop.pojo.Type;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by tao on 2017/4/28 0028.
+ */
+@WebServlet(name = "GetTypeByIdServlet",urlPatterns = "/getTypeById")
+public class GetTypeByIdServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try{
+            TypeDao ad = new TypeDaoImpl();
+            String paramId = request.getParameter("id");
+            Integer id = Integer.parseInt(paramId);
+            Type type = ad.get(id);
+            PrintWriter out = response.getWriter();
+            //将list的数据转换成JSON返回给前台
+            //JSON.DEFFAULT_DATE_FORMAT = "yyyy-MM-dd";
+            //SerializerFeature.WriteDateUseDateFormat用来将日期格式化成yyyy-MM-dd的形式
+            String json = JSON.toJSONString(type, SerializerFeature.WriteDateUseDateFormat,SerializerFeature.PrettyFormat, SerializerFeature.DisableCircularReferenceDetect);
+            if(type != null) {
+                out.println(json);
+                out.close();
+            }else{
+                out.println(false);
+                out.close();
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request,response);
+    }
+}
--- ishop-manager/src/test/java/com/ishop/dao/impl/TypeDaoImplTest.java
+++ ishop-manager/src/test/java/com/ishop/dao/impl/TypeDaoImplTest.java
@@ -1,10 +1,10 @@
-package com.ishop.dao.impl; 
+package com.ishop.dao.impl;
 
 import com.ishop.dao.TypeDao;
 import com.ishop.pojo.Type;
-import org.junit.Test;
-import org.junit.Before; 
 import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,297 +12,7 @@ import java.util.List;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-/** 
-* TypeDaoImpl Tester. 
-* 
-* @author <Authors name> 
-* @since <pre>���� 28, 2017</pre> 
-* @version 1.0 
-*/ 
-public class TypeDaoImplTest { 
-
-@Before
-public void before() throws Exception { 
-} 
-
-@After
-public void after() throws Exception { 
-} 
-
-/** 
-* 
-* Method: add(Type type) 
-* 
-*/ 
-@Test
-public void testAdd() throws Exception { 
-//TODO: Test goes here...
-    //测试成功
-//    Type type = new Type();
-//    type.setParentId(1);
-//    type.setName("test");
-//    type.setDescribe("hh");
-//    type.setGrade(2);
-//    TypeDao td = new TypeDaoImpl();
-//    boolean result = td.add(type);
-//    assertTrue(result);
-} 
-
-/** 
-* 
-* Method: update(Type type) 
-* 
-*/ 
-@Test
-public void testUpdate() throws Exception { 
-//TODO: Test goes here... 
-} 
-
-/** 
-* 
-* Method: delete(Integer id) 
-* 
-*/ 
-@Test
-public void testDelete() throws Exception { 
-//TODO: Test goes here... 
-} 
-
-/** 
-* 
-* Method: get(Integer id) 
-* 
-*/ 
-@Test
-public void testGet() throws Exception { 
-//TODO: Test goes here... 
-} 
-
-/** 
-* 
-* Method: getTotal() 
-* 
-*/ 
-@Test
-public void testGetTotal() throws Exception { 
-//TODO: Test goes here... 
-} 
-
-/** 
-* 
-* Method: list() 
-* 
-*/ 
-@Test
-public void testList() throws Exception { 
-//TODO: Test goes here...
-    TypeDao td = new TypeDaoImpl();
-    List<Type> list = td.list();
-    for(Type type : list){
-        System.out.println(type);
-    }
-    assertNotNull(list);
-} 
-
-/** 
-* 
-* Method: list(int start, int count) 
-* 
-*/ 
-@Test
-public void testListForStartCount() throws Exception { 
-//TODO: Test goes here... 
-} 
-
-/** 
-* 
-* Method: exists(Integer id) 
-* 
-*/ 
-@Test
-public void testExists() throws Exception { 
-//TODO: Test goes here... 
-} 
-
-/** 
-* 
-* Method: parentList(int grade) 
-* 
-*/ 
-@Test
-public void testParentList() throws Exception { 
-//TODO: Test goes here...
-    TypeDao td = new TypeDaoImpl();
-    List<Type> list= new ArrayList<Type>();
-    int childGrade = 2;
-    list = td.getParentList(2);
-    for(Type t : list){
-        System.out.println(t);
-    }
-    assertNotNull(list);
-} 
-
-
-} 
-
-import com.ishop.dao.TypeDao;
-import com.ishop.pojo.Type;
-import org.junit.Test;
-import org.junit.Before; 
-import org.junit.After;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
-/** 
-* TypeDaoImpl Tester. 
-* 
-* @author <Authors name> 
-* @since <pre>���� 28, 2017</pre> 
-* @version 1.0 
-*/ 
-public class TypeDaoImplTest { 
-
-@Before
-public void before() throws Exception { 
-} 
-
-@After
-public void after() throws Exception { 
-} 
-
-/** 
-* 
-* Method: add(Type type) 
-* 
-*/ 
-@Test
-public void testAdd() throws Exception { 
-//TODO: Test goes here...
-    //测试成功
-//    Type type = new Type();
-//    type.setParentId(1);
-//    type.setName("test");
-//    type.setDescribe("hh");
-//    type.setGrade(2);
-//    TypeDao td = new TypeDaoImpl();
-//    boolean result = td.add(type);
-//    assertTrue(result);
-} 
-
-/** 
-* 
-* Method: update(Type type) 
-* 
-*/ 
-@Test
-public void testUpdate() throws Exception { 
-//TODO: Test goes here... 
-} 
-
-/** 
-* 
-* Method: delete(Integer id) 
-* 
-*/ 
-@Test
-public void testDelete() throws Exception { 
-//TODO: Test goes here... 
-} 
-
-/** 
-* 
-* Method: get(Integer id) 
-* 
-*/ 
-@Test
-public void testGet() throws Exception { 
-//TODO: Test goes here... 
-} 
-
-/** 
-* 
-* Method: getTotal() 
-* 
-*/ 
-@Test
-public void testGetTotal() throws Exception { 
-//TODO: Test goes here... 
-} 
-
-/** 
-* 
-* Method: list() 
-* 
-*/ 
-@Test
-public void testList() throws Exception { 
-//TODO: Test goes here...
-    TypeDao td = new TypeDaoImpl();
-    List<Type> list = td.list();
-    for(Type type : list){
-        System.out.println(type);
-    }
-    assertNotNull(list);
-} 
-
-/** 
-* 
-* Method: list(int start, int count) 
-* 
-*/ 
-@Test
-public void testListForStartCount() throws Exception { 
-//TODO: Test goes here... 
-} 
-
-/** 
-* 
-* Method: exists(Integer id) 
-* 
-*/ 
-@Test
-public void testExists() throws Exception { 
-//TODO: Test goes here... 
-} 
-
-/** 
-* 
-* Method: parentList(int grade) 
-* 
-*/ 
-@Test
-public void testParentList() throws Exception { 
-//TODO: Test goes here...
-    TypeDao td = new TypeDaoImpl();
-    List<Type> list= new ArrayList<Type>();
-    int childGrade = 2;
-    list = td.getParentList(2);
-    for(Type t : list){
-        System.out.println(t);
-    }
-    assertNotNull(list);
-} 
-
-
-} 
-
-import com.ishop.dao.TypeDao;
-import com.ishop.pojo.Type;
-import org.junit.Test;
-import org.junit.Before; 
-import org.junit.After;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
-/** 
+/**
 * TypeDaoImpl Tester. 
 * 
 * @author <Authors name> 
@@ -371,7 +81,14 @@ public void testDelete() throws Exception {
 public void testGet() throws Exception { 
 //TODO: Test goes here... 
 } 
-
+@Test
+public void testGetById() throws Exception{
+    TypeDao td = new TypeDaoImpl();
+    int id = 1;
+    Type type = td.get(id);
+    System.out.println(type);
+    assertNotNull(type);
+}
 /** 
 * 
 * Method: getTotal() 
@@ -396,9 +113,9 @@ public void testList() throws Exception {
         System.out.println(type);
     }
     assertNotNull(list);
-} 
+}
 
-/** 
+/**
 * 
 * Method: list(int start, int count) 
 * 
