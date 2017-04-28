36d76ccda5101f81afe47840a835c235e34f75ce
myq <84210278@qq.com>
2017-04-28 19:26:38 +0800

    admin 完成DeleteUserServlet.java

--- ishop-admin/src/main/java/com/ishop/servlet/user/DeleteUserServlet.java
+++ ishop-admin/src/main/java/com/ishop/servlet/user/DeleteUserServlet.java
@@ -10,13 +10,14 @@ import java.io.IOException;
 /**
  * Created by tao on 2017/4/22 0022.
  */
-@WebServlet(name = "DeleteUserServlet")
 public class DeleteUserServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        try{
+            UserDao ud = new UserDaoImpl();
+            String paramId = request.getParameter("id");
+            Integer id = Integer.parseInt(paramId);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
     }
 }
