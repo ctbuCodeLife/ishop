6e8f614eacaec9b5396f59833a43975c2deb3a0e
myq <84210278@qq.com>
2017-04-28 19:27:25 +0800

    admin  修改 DeleteUserServlet.java

--- ishop-admin/src/main/java/com/ishop/servlet/user/DeleteUserServlet.java
+++ ishop-admin/src/main/java/com/ishop/servlet/user/DeleteUserServlet.java
@@ -1,23 +1,44 @@
 package com.ishop.servlet.user;
 
+import com.ishop.dao.UserDao;
+import com.ishop.dao.impl.UserDaoImpl;
+import com.ishop.pojo.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Created by tao on 2017/4/22 0022.
  */
+@WebServlet(name = "DeleteUserServlet",urlPatterns = "/delUser")
 public class DeleteUserServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try{
             UserDao ud = new UserDaoImpl();
             String paramId = request.getParameter("id");
             Integer id = Integer.parseInt(paramId);
+            boolean result = ud.delete(id);
+            PrintWriter out = response.getWriter();
+            String message="";
+            if(result == true){
+                message = "delect sucess!";
+            }else{
+                message = "delete failure!";
+            }
+            out.println(message);
+            out.close();
+            System.out.print(1111);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request,response);
     }
 }
